573b8494e14567cddc465d7cc30b41afb47c0c07
jjsendor <jjsendor@gmail.com>
2013-12-19 11:21:09 +0100

    Added initial solution using recursion

--- src/max_subarray/core.clj
+++ src/max_subarray/core.clj
@@ -1,6 +1,22 @@
+; Solves the maximum subarray problem:
+; http://en.wikipedia.org/wiki/Maximum_subarray_problem
+
 (ns max-subarray.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn find-max-subarray
+	"Helper function that finds the maximum subarray given a maximum subarray found so far by iterating over an array."
+	[a i max_sum max_begin max_end sum sum_begin]
+	(if (empty? a)
+		(list max_begin max_end)
+		(let [new_sum (+ sum (first a))] ; could it be done without using "let" here?
+			(if (> new_sum max_sum)
+				(find-max-subarray (rest a) (inc i) new_sum sum_begin i new_sum sum_begin)
+				(if (< new_sum 0) ; the second comparision will not work well if we have an array with only negative numbers
+					(find-max-subarray (rest a) (inc i) max_sum max_begin max_end 0 (inc i))
+					(find-max-subarray (rest a) (inc i) max_sum max_begin max_end new_sum sum_begin))))))
+
+
+(defn max-subarray
+  "Returns begin and end index of the maximum subarray for a given array (passed here as a list)."
+  [a]
+  (find-max-subarray a 0 0 0 0 0 0))
--- test/max_subarray/core_test.clj
+++ test/max_subarray/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [max-subarray.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest max-subarray-test1
+  (testing "Example array: [ 1, 3, -8, 2, -1, 10, -9 ]"
+    (is (= (max-subarray '(1, 3, -8, 2, -1, 10, -9)) '(3 5)))))
+
+(deftest max-subarray-test2
+  (testing "Example array: [ 1, 3, -8 ]"
+    (is (= (max-subarray '(1, 3, -8)) '(0 1)))))
+
+(deftest max-subarray-test3
+  (testing "Example array: [ 1, 3, -8, 2 ]"
+    (is (= (max-subarray '(1, 3, -8, 2)) '(0 1)))))
