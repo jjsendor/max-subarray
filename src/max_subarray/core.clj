47b581df99fb5fb90a1f6fd4561de2733a4226ea
jjsendor <jjsendor@gmail.com>
2013-12-19 10:31:22 +0100

    Empty project import

---
+++ src/max_subarray/core.clj
@@ -0,0 +1,6 @@
+(ns max-subarray.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/max_subarray/core_test.clj
@@ -0,0 +1,7 @@
+(ns max-subarray.core-test
+  (:require [clojure.test :refer :all]
+            [max-subarray.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
